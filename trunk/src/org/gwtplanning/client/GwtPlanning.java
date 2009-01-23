94b63a9b603e092bae14826abda68e4aca2340d3
stephane.mallet <stephane.mallet@29219e6a-e22c-11dd-a893-8d7ce9fbef62>
2009-01-23 10:16:06 +0000

    Remove Talend headers in all classes

--- trunk/src/org/gwtplanning/client/GwtPlanning.java
+++ trunk/src/org/gwtplanning/client/GwtPlanning.java
@@ -1,19 +1,6 @@
-// ============================================================================
-//
-// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
-//
-// This source code is available under agreement available at
-// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
-//
-// You should have received a copy of the agreement
-// along with this program; if not, write to Talend SA
-// 9 rue Pages 92150 Suresnes, France
-//
-// ============================================================================
 package org.gwtplanning.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -21,8 +8,6 @@ import com.google.gwt.user.client.ui.RootPanel;
 public class GwtPlanning implements EntryPoint {
 
     public void onModuleLoad() {
-        PlanningParameters parameters = new PlanningParameters();
-        RootPanel.get().add(parameters);
     }
 
 }
