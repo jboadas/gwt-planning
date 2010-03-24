bd209ebdc052437e104ab14e3450f4850f00eb51
stephane.mallet <stephane.mallet@29219e6a-e22c-11dd-a893-8d7ce9fbef62>
2010-03-24 09:55:28 +0000

    Modification: centralize date pattern and format in Constants.java

--- trunk/src/org/gwtplanning/client/EventDetailsDecorator.java
+++ trunk/src/org/gwtplanning/client/EventDetailsDecorator.java
@@ -4,7 +4,6 @@ import org.gwtplanning.client.i18n.PlanningMessages;
 import org.gwtplanning.client.model.Event;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.ui.Label;
 import com.gwtext.client.widgets.Panel;
 
@@ -22,13 +21,11 @@ public interface EventDetailsDecorator {
 
         private static PlanningMessages messages = (PlanningMessages) GWT.create(PlanningMessages.class);
 
-        private static DateTimeFormat dateTimeFormat = DateTimeFormat.getFormat("yyyy-MM-dd H:mm");
-
         public void decorate(Event event, Panel panel) {
             panel.setWidth(250);
             panel.setHeight(150);
-            panel.add(new Label(messages.eventDetails_startDate(dateTimeFormat.format(event.getStart()))));
-            panel.add(new Label(messages.eventDetails_endDate(dateTimeFormat.format(event.getEnd()))));
+            panel.add(new Label(messages.eventDetails_startDate(Constants.DATE_FORMAT.format(event.getStart()))));
+            panel.add(new Label(messages.eventDetails_endDate(Constants.DATE_FORMAT.format(event.getEnd()))));
             long elapsedTime = (event.getEnd().getTime() - event.getStart().getTime()) / 1000 / 60;
             panel.add(new Label(messages.eventDetails_elapsedTime(elapsedTime + "")));
             // panel.add(new Label("Status: " + event.getStatus()));
