501dc250293c8538670251f66496da6197c4faba
stephane.mallet <stephane.mallet@29219e6a-e22c-11dd-a893-8d7ce9fbef62>
2010-03-24 10:20:31 +0000

    Feature 1 added: Evaluate automatically the period

--- src/org/gwtplanning/client/PlanningParameters.java
+++ src/org/gwtplanning/client/PlanningParameters.java
@@ -13,18 +13,13 @@ import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.gwtext.client.core.EventObject;
-import com.gwtext.client.data.Record;
-import com.gwtext.client.data.SimpleStore;
-import com.gwtext.client.data.Store;
 import com.gwtext.client.util.DateUtil;
 import com.gwtext.client.widgets.Button;
 import com.gwtext.client.widgets.Panel;
 import com.gwtext.client.widgets.event.ButtonListenerAdapter;
-import com.gwtext.client.widgets.form.ComboBox;
 import com.gwtext.client.widgets.form.DateField;
 import com.gwtext.client.widgets.form.Field;
 import com.gwtext.client.widgets.form.TimeField;
-import com.gwtext.client.widgets.form.event.ComboBoxListenerAdapter;
 import com.gwtext.client.widgets.form.event.TextFieldListenerAdapter;
 import com.gwtext.client.widgets.layout.HorizontalLayout;
 
@@ -34,12 +29,6 @@ import com.gwtext.client.widgets.layout.HorizontalLayout;
 public class PlanningParameters extends SimplePanel {
 
     private static PlanningMessages messages = (PlanningMessages) GWT.create(PlanningMessages.class);
-
-    private static final String COMBO_ID_FIELD = "id";
-
-    private static final String COMBO_LABEL_FIELD = "label";
-
-    private ComboBox scaleCombo;
 
     private TimeField startTimeField;
 
@@ -59,26 +48,6 @@ public class PlanningParameters extends SimplePanel {
         formPanel.setWidth(900);
         formPanel.setBorder(true);
         formPanel.setLayout(new HorizontalLayout(5));
-
-        formPanel.add(new Label(messages.parameters_scale() + ":", false));
-
-        final Store store = new SimpleStore(new String[] { COMBO_ID_FIELD, COMBO_LABEL_FIELD }, getScales());
-        store.load();
-
-        scaleCombo = new ComboBox();
-        scaleCombo.setForceSelection(true);
-        scaleCombo.setMinChars(1);
-        scaleCombo.setFieldLabel(messages.parameters_scale());
-        scaleCombo.setStore(store);
-        scaleCombo.setValueField(COMBO_ID_FIELD);
-        scaleCombo.setDisplayField(COMBO_LABEL_FIELD);
-        scaleCombo.setMode(ComboBox.LOCAL);
-        scaleCombo.setTriggerAction(ComboBox.ALL);
-        scaleCombo.setTypeAhead(true);
-        scaleCombo.setSelectOnFocus(true);
-        scaleCombo.setWidth(100);
-
-        formPanel.add(scaleCombo);
 
         formPanel.add(new Label(messages.parameters_startDate() + ":", false));
         startDateField = new DateField(messages.parameters_startDate(), "startDate", 80);
@@ -117,21 +86,11 @@ public class PlanningParameters extends SimplePanel {
     }
 
     private void setDefaultValues() {
-        scaleCombo.setValue(ScaleMode.FOUR_HOUR.toString());
         startTimeField.setValue("0:00");
         endTimeField.setValue("0:00");
     }
 
     private void addListeners() {
-        scaleCombo.addListener(new ComboBoxListenerAdapter() {
-
-            @Override
-            public void onSelect(ComboBox comboBox, Record record, int index) {
-                fireChange();
-            }
-
-        });
-
         TextFieldListenerAdapter listener = new TextFieldListenerAdapter() {
 
             @Override
@@ -167,21 +126,9 @@ public class PlanningParameters extends SimplePanel {
         });
     }
 
-    private String[][] getScales() {
-        String[][] toReturn = new String[ScaleMode.values().length][2];
-
-        int i = 0;
-        for (ScaleMode current : ScaleMode.values()) {
-            toReturn[i][0] = current.toString();
-            toReturn[i][1] = current.getLabel();
-            i++;
-        }
-        return toReturn;
-    }
-
     public ScaleMode getScaleMode() {
-        String value = scaleCombo.getText();
-        return ScaleMode.get(value);
+        long diff = getEndDate().getTime() - getStartDate().getTime();
+        return ScaleMode.get(diff);
     }
 
     public Date getStartDate() {
